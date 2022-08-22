eb6f3caed1945aa02669fd23d4d3c86d11bcd907
wooldridge <mike@hellion.org>
2022-08-22 15:46:04 -0700

    Check if ResultActions links empty

--- src/components/ResultActions/ResultActions.js
+++ src/components/ResultActions/ResultActions.js
@@ -33,24 +33,27 @@ const ResultActions = (props) => {
   const getActions = () => {
     let actionsData = getValByConfig(data, config);
     actionsData = _.isNil(actionsData) ? null : (Array.isArray(actionsData) ? actionsData : [actionsData]);
-    const links = actionsData.map((action, index) => {
-      const {action: {icon = null, color = null, url = null}} = config;
-      if (!icon) return;
-      let iconValue = getValByPath(action, icon, true);
-      let colorValue = getValByPath(action, color, true);
-      let urlValue = getValByPath(action, url, true);
-      let iconElement = FaDictionary[iconValue];
+    let links = [];
+    if (actionsData) {
+      links = actionsData.map((action, index) => {
+        const {action: {icon = null, color = null, url = null}} = config;
+        if (!icon) return;
+        let iconValue = getValByPath(action, icon, true);
+        let colorValue = getValByPath(action, color, true);
+        let urlValue = getValByPath(action, url, true);
+        let iconElement = FaDictionary[iconValue];
 
-      let iconClassName = props.className ? props.className : props.config.className ? props.config.className : "";
-      let iconStyle = props.style ? props.style : props.config.style ? props.config.style : {};
-      iconStyle = {...iconStyle, color: colorValue ? colorValue : '#5d6aaa'};
+        let iconClassName = props.className ? props.className : props.config.className ? props.config.className : "";
+        let iconStyle = props.style ? props.style : props.config.style ? props.config.style : {};
+        iconStyle = {...iconStyle, color: colorValue ? colorValue : '#5d6aaa'};
 
-      return (
-        <a href={urlValue ? urlValue : "#"} target="_blank" className="ResultActions-item" key={index} data-testid={`action-item-${index}`}>
-          <span className={iconClassName} style={iconStyle}><FontAwesomeIcon icon={iconElement} /></span>
-        </a>
-      )
-    })
+        return (
+          <a href={urlValue ? urlValue : "#"} target="_blank" className="ResultActions-item" key={index} data-testid={`action-item-${index}`}>
+            <span className={iconClassName} style={iconStyle}><FontAwesomeIcon icon={iconElement} /></span>
+          </a>
+        )
+      })
+    }
 
     return links;
   }
